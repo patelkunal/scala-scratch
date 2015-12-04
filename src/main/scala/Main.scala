d55354fa7c0404d02bd308569b08eb2414ac356b
Kunal Patel <kunal_patel@symantec.com>
2015-12-04 16:07:54 +0530

    add various types of method to implement

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,29 @@
-object Main extends App {
-	println("hello from scala and gradle !!");
+object Main {
+	
+	def main(args: Array[String]) {
+		// args.foreach(println)
+		val n: Int = 7
+		greet();
+		println(square(n))
+		println(squareWithBlock(n))
+		println(squareWithBlockAndReturnType(n))
+		println(squareWithBlockAndReturnTypeAndReturnStmt(n))
+	}
+
+	def greet(): Unit = println("hello, world !!")
+
+	def square(a: Int) = a * a
+
+	def squareWithBlock(a: Int) = {
+		a * a
+	}
+	
+	def squareWithBlockAndReturnType(a: Int): Int = {
+		a * a
+	}
+	
+	def squareWithBlockAndReturnTypeAndReturnStmt(a: Int): Int = {
+		return a * a
+	}
 }
 

2ce1bb11d336abb2b1a44da83285a91ab3aff4d4
Kunal Patel <kunal_patel@symantec.com>
2015-12-21 15:05:45 +0530

    separate test case based on inputs

--- src/test/scala/pack/calc/CalculatorTest.scala
+++ src/test/scala/pack/calc/CalculatorTest.scala
@@ -2,7 +2,7 @@ package pack.calc
 
 import org.junit.{After, Before, Test}
 
-import scala.collection.immutable.Range.inclusive
+import scala.collection.immutable.Range
 
 /**
   * Created by kunal_patel on 12/12/15.
@@ -22,30 +22,28 @@ class CalculatorTest {
 	}
 
 	@Test
-	def testGetRangeList(): Unit = {
-		val list = calc.getRangeList(1, 5)
-		assert(list == inclusive(1, 5).toList, "Lists should be equal !!")
-	}
-
-	@Test
 	def testSumIntListBad_withoutStackoverFlow(): Unit = {
-		val sum = calc.sumIntListBad(inclusive(1, 5).toList)
+		val sum = calc.sumIntListBad(Range.inclusive(1, 5).toList)
 		assert(15 == sum)
 	}
 
 	@Test(expected = classOf[StackOverflowError])
 	def testSumIntListBad_withStackoverFlow(): Unit = {
-		assert(50005000 == calc.sumIntListBad(inclusive(1, 10000).toList))
+		assert(50005000 == calc.sumIntListBad(Range.inclusive(1, 10000).toList))
 	}
 
 	@Test
-	def testSumIntListGood(): Unit = {
-		assert(50005000 == calc.sumIntListGood(inclusive(1, 10000).toList))
-		assert(5000050000L == calc.sumIntListGood(inclusive(1, 100000).toList))
-		assert(500000500000L == calc.sumIntListGood(inclusive(1, 1000000).toList))
-		assert(50000005000000L == calc.sumIntListGood(inclusive(1, 10000000).toList))
-		assert(5000000050000000L == calc.sumIntListGood(inclusive(1, 100000000).toList))
-		assert(500000000500000000L == calc.sumIntListGood(inclusive(1, 1000000000).toList))
+	def testSumIntListGood_upto10MList(): Unit = {
+		assert(50005000 == calc.sumIntListGood(Range.inclusive(1, 10000).toList))
+		assert(5000050000L == calc.sumIntListGood(Range.inclusive(1, 100000).toList))
+		assert(500000500000L == calc.sumIntListGood(Range.inclusive(1, 1000000).toList))
+		assert(50000005000000L == calc.sumIntListGood(Range.inclusive(1, 10000000).toList))
+	}
+
+	@Test(timeout = 1000000)
+	def testSumIntListGood_upto1BList(): Unit = {
+		assert(5000000050000000L == calc.sumIntListGood(Range.inclusive(1, 100000000).toList))
+		assert(500000000500000000L == calc.sumIntListGood(Range.inclusive(1, 1000000000).toList))
 	}
 
 }
